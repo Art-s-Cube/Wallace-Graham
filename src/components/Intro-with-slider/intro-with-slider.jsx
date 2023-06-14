914589c338aae828a576131ee5ea519d8993b1ab
wjsacken <wsackenheim@artscube.biz>
2023-06-14 09:44:15 -0400

    rendering

--- src/components/Intro-with-slider/intro-with-slider.jsx
+++ src/components/Intro-with-slider/intro-with-slider.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
-import introData from "data/sections/intro.json";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import SwiperCore, { Navigation, Pagination, Parallax } from "swiper";
-
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -14,13 +11,9 @@ import fadeWhenScroll from "common/fadeWhenScroll";
 SwiperCore.use([Navigation, Pagination, Parallax]);
 
 const IntroWithSlider = ({ sliderRef }) => {
-  const [load, setLoad] = React.useState(true);
   React.useEffect(() => {
     fadeWhenScroll(document.querySelectorAll(".fixed-slider .caption"));
-    setTimeout(() => {
-      removeSlashFromPagination();
-      setLoad(false);
-    }, 100);
+    removeSlashFromPagination();
   }, []);
 
   const navigationPrevRef = React.useRef(null);
@@ -28,80 +21,135 @@ const IntroWithSlider = ({ sliderRef }) => {
   const paginationRef = React.useRef(null);
 
   return (
-    <header
-      ref={sliderRef}
-      className="slider slider-prlx fixed-slider text-center"
-    >
+    <header ref={sliderRef} className="slider slider-prlx fixed-slider text-center">
       <div className="swiper-container parallax-slider">
-        {!load ? (
-          <Swiper
-            speed={1000}
-            parallax={true}
-            navigation={{
-              prevEl: navigationPrevRef.current,
-              nextEl: navigationNextRef.current,
-            }}
-            pagination={{
-              type: "fraction",
-              clickable: true,
-              el: paginationRef.current,
-            }}
-            onBeforeInit={(swiper) => {
+        <Swiper
+          parallax={true}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+          pagination={{
+            type: "fraction",
+            clickable: true,
+            el: paginationRef.current,
+          }}
+          onBeforeInit={(swiper) => {
+            swiper.params.navigation.prevEl = navigationPrevRef.current;
+            swiper.params.navigation.nextEl = navigationNextRef.current;
+            swiper.params.pagination.el = paginationRef.current;
+          }}
+          onSwiper={(swiper) => {
+            setTimeout(() => {
+              for (var i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].childNodes[0].setAttribute(
+                  "data-swiper-parallax",
+                  0.75 * swiper.width
+                );
+              }
+
               swiper.params.navigation.prevEl = navigationPrevRef.current;
               swiper.params.navigation.nextEl = navigationNextRef.current;
+
               swiper.params.pagination.el = paginationRef.current;
-            }}
-            onSwiper={(swiper) => {
-              setTimeout(() => {
-                for (var i = 0; i < swiper.slides.length; i++) {
-                  swiper.slides[i].childNodes[0].setAttribute(
-                    "data-swiper-parallax",
-                    0.75 * swiper.width
-                  );
-                }
 
-                swiper.params.navigation.prevEl = navigationPrevRef.current;
-                swiper.params.navigation.nextEl = navigationNextRef.current;
+              swiper.navigation.destroy();
+              swiper.navigation.init();
+              swiper.navigation.update();
 
-                swiper.params.pagination.el = paginationRef.current;
-
-                swiper.navigation.destroy();
-                swiper.navigation.init();
-                swiper.navigation.update();
-
-                swiper.pagination.destroy();
-                swiper.pagination.init();
-                swiper.pagination.update();
-              });
-            }}
-            className="swiper-wrapper"
-            slidesPerView={1}
-          >
-            {introData.map((slide) => (
-              <SwiperSlide key={slide.id} className="swiper-slide">
-                <div
+              swiper.pagination.destroy();
+              swiper.pagination.init();
+              swiper.pagination.update();
+            });
+          }}
+          className="swiper-wrapper"
+          slidesPerView={1}
+        >
+            <SwiperSlide key="1" className="swiper-slide">
+              <div
                   className="bg-img valign"
-                  style={{ backgroundImage: `url(${slide.image})` }}
+                  style={{ backgroundImage: `url(/img/slid/team.jpg)` }}
                   data-overlay-dark="6"
-                >
+              >
                   <div className="container">
-                    <div className="row justify-content-center">
+                  <div className="row justify-content-center">
                       <div className="col-lg-8 col-md-10">
-                        <div className="caption center mt-30">
-                          <h1 className="">{slide.title}</h1>
-                          {slide?.content && <p>{slide.content}</p>}
+                      <div className="caption center mt-30">
+                          <h1 className="">Our Clients Are Our Priority</h1>
+                          <p>Our firm motto says it all.  We listen.  We care.  We help.  We aim to improve the lives and well-being of each and every client that walks through our door.</p>
                           <Link to="/about/about-dark" className="butn bord curve mt-30">
-                            <span>Look More</span>
+                          <span>Look More</span>
                           </Link>
-                        </div>
                       </div>
-                    </div>
+                      </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            ))}
+                  </div>
+              </div>
+          </SwiperSlide>
+          <SwiperSlide key="2" className="swiper-slide">
+              <div
+                  className="bg-img valign"
+                  style={{ backgroundImage: `url(/img/slid/mona.jpg)` }}
+                  data-overlay-dark="6"
+              >
+                  <div className="container">
+                  <div className="row justify-content-center">
+                      <div className="col-lg-8 col-md-10">
+                      <div className="caption center mt-30">
+                          <h1 className="">Mona Lisa Wallace</h1>
+                          <p>Mona Lisa founded our firm in 1981, and has been working tirelessly ever since in her fight for fairness and justice.  She has received numerous awards and acknowledgements for her successful representation of thousands of injured and harmed employees and consumers nationwide.</p>
+                          <Link to="/about/about-dark" className="butn bord curve mt-30">
+                          <span>Look More</span>
+                          </Link>
+                      </div>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+          </SwiperSlide>
+          <SwiperSlide key="3" className="swiper-slide">
+              <div
+                  className="bg-img valign"
+                  style={{ backgroundImage: `url(/img/slid/bill.jpg)` }}
+                  data-overlay-dark="6"
+              >
+                  <div className="container">
+                  <div className="row justify-content-center">
+                      <div className="col-lg-8 col-md-10">
+                      <div className="caption center mt-30">
+                          <h1 className="">Bill Graham</h1>
+                          <p>Bill uses his compassion and decades of legal experience to help individuals </p>
+                          <Link to="/about/about-dark" className="butn bord curve mt-30">
+                          <span>Look More</span>
+                          </Link>
+                      </div>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+          </SwiperSlide>
+          <SwiperSlide key="4" className="swiper-slide">
+              <div
+                  className="bg-img valign"
+                  style={{ backgroundImage: `url(/img/slid/whit.jpg)` }}
+                  data-overlay-dark="6"
+              >
+                  <div className="container">
+                  <div className="row justify-content-center">
+                      <div className="col-lg-8 col-md-10">
+                      <div className="caption center mt-30">
+                          <h1 className="">We Love What We Do</h1>
+                          <p>We find joy in investigating legal harms and to potentially helping another deserving client. </p>
+                          <Link to="/about/about-dark" className="butn bord curve mt-30">
+                          <span>Look More</span>
+                          </Link>
+                      </div>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+          </SwiperSlide>
           </Swiper>
-        ) : null}
         <div className="setone setwo">
           <div
             ref={navigationNextRef}
@@ -120,5 +168,4 @@ const IntroWithSlider = ({ sliderRef }) => {
     </header>
   );
 };
-
 export default IntroWithSlider;
