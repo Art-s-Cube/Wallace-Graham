c848697cb9af9782cd363404dca6113f09984cdc
wjsacken <wsackenheim@artscube.biz>
2023-06-21 15:11:26 -0400

    light.css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,11 +11,7 @@ const HeadComponents = [
   <meta key="keywords" name="keywords" content="personal injury lawyers north carolina, personal injury lawyer salisbury, personal injury attorneys salisbury, personal injury lawyer kannapolis, personal injury lawyer concord, mesothelioma lawyers, Cancer lawyers, workers compensation north carolina, social security disability attorney salisbury nc, social security disability lawyer salisbury nc" />,
   <meta key="description" name="description" content="Wallace and Graham a law firm in the small town of Salisbury, North Carolina." />,
   <meta key="author" name="author" content="Arts Cube" />,
-  // Google Fonts
-  //<link key="Poppins" media="print" onLoad="this.onload=null;this.removeAttribute('media');" rel="preconnect" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" crossOrigin="true"/>,
-  //<link key="Montserrat" media="print" onLoad="this.onload=null;this.removeAttribute('media');" rel="preconnect" href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap" crossOrigin="true"/>,
-  // Styles
-  //<link key="ionicons" rel="stylesheet" href="/css/ionicons.min.css" />,
+
   <link key="styles-light" rel="stylesheet" href="/css/light.min.css" />,
   <link key="boot" rel="stylesheet" href="/css/bootstrap.min.css" />,
   <link key="animate" rel="stylesheet" href="/css/animate.css" />,
@@ -26,9 +22,7 @@ const HeadComponents = [
   <link key="icon" rel="shortcut icon" href="/img/favicon.ico" title="Favicon" sizes="16x16" />,
   // Scripts
   <Script key="isotope" src="/js/isotope.pkgd.min.js"></Script>,
-  //<script key="splitting" src="/js/splitting.min.js"></script>,
-  //<Script key="simpleParallax" src="/js/simpleParallax.min.js"></Script>,
-  <script key="wow" src="/js/wow.min.js"></script>,
+<script key="wow" src="/js/wow.min.js"></script>,
 ]
 
 export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
